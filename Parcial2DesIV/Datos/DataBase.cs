ad2bdde515ca0370a039c365d2080aa4ebec5dcc
Charlie <cechevers0620@gmail.com>
2025-11-09 00:56:06 -0500

    Add DataBase class for PostgreSQL connection
    Introduces a new DataBase class in the Datos namespace to manage PostgreSQL connections using Npgsql. Also adds configuration for the connection string in Parcial2DesIV.exe.config.

---
+++ Parcial2DesIV/Datos/DataBase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parcial2DesIV.Datos
+{
+    public class DataBase
+    {
+        private Npgsql.NpgsqlConnection _con;
+        Npgsql.NpgsqlCommand _cmd;
+        Npgsql.NpgsqlDataAdapter _adapter;
+
+        DataSet _ds;
+
+        public DataBase()
+        {
+            _con = new Npgsql.NpgsqlConnection();
+            _con.ConnectionString = ConfigurationManager.ConnectionStrings["cad_con"].ConnectionString;
+            _cmd = new Npgsql.NpgsqlCommand();
+            _cmd.Connection = _con;
+        }
+    }
+}
